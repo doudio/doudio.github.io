c78dcdd5d9140f1b1159571133253bcf3c5399e1
quwenlei <qwl@123@secxun.com>
2023-06-16 11:03:17 +0800

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230616.105131';
+const cacheName = 'chirpy-20230616.110235';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
