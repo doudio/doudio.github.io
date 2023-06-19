d37053327c64dea4e4195966363cc602be2212e7
豆帝 <2491920818@qq.com>
2023-06-19 11:03:50 +0800

    init project

---
+++ blog-script/src/main/java/com/example/blogscript/BlogScriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogScriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogScriptApplication.class, args);
+    }
+
+}
